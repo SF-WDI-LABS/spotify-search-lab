a630a31128fcf0101832f8db8ea6bb7c1192835b
Nathan Allen <thenathanator@gmail.com>
2016-03-22 16:11:29 -0700

    Revert "readme and starter code changes - and solution-handlebars"

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+// wait for DOM to load before running JS
+$(document).on('ready', function() {
+
+  // check to make sure JS is loaded
+  console.log('JS is loaded!');
+
+  // your code here
+
+});
