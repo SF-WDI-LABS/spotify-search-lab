65bdb08c78e93a085778578f7d4c2c392557a05c
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-02-04 19:28:03 -0800

    successfully manipulated dom to include spotify track data

--- js/app.js
+++ js/app.js
@@ -1,9 +1,75 @@
-// wait for DOM to load before running JS
-$(document).on('ready', function() {
+  // wait for DOM to load before running JS
+  $(document).on('ready', function() {
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
+    // check to make sure JS is loaded
+    console.log('JS is loaded!');
 
-  // your code here
+    $.ajax({
+      method: 'GET',
+      // url: `https://api.spotify.com/v1/tracks/${trackId}`,
+      url: `https://api.spotify.com/v1/search?q=changes&type=track&market=US`,
+      dataType: 'json',
+      success: onSuccess
+    });
 
-});
+  }); //end ajax call on document load
+
+  var data;
+    //ajax data set in global scope
+
+  function onSuccess(responseData){
+      console.log(responseData);
+      data = responseData;
+
+  // BEGIN LOOPING
+    for (var i=1; i<6; i++){
+
+      albumCover = data.tracks.items[i].album.images[1].url;
+      console.log(albumCover);
+      artist = data.tracks.items[i].album.artists[0].name;
+      track = data.tracks.items[i].name;
+      sample = data.tracks.items[i].preview_url;
+
+      var varNewSection = `<div class="row result">
+        <div class="album-cover">
+          <img id="cover-${[i]}" src="images/album-placeholder.jpg" width="300px">
+        </div>
+
+        <div class="right-result">
+          <div class="song-artist">
+            <h3 id="artist-${[i]}">Artist</h3>
+            <h4 id="track-${[i]}">Track</h3>
+          </div>
+          <button id="sample-${[i]}"><a href="#">Play${[i]}</a></button>
+        </div>
+      </div>  <!-- -row-result close -->
+
+      <hr>`;
+
+      function appendHtml(){
+      $(".container").append(varNewSection);
+      }
+      appendHtml();
+
+      function appendCover(){
+        $(`#cover-${i}`).attr("src", albumCover);
+      };
+      appendCover();
+
+      function appendArtist(){
+        $(`#artist-${i}`).text(artist);
+      };
+      appendArtist();
+
+      function appendTrack(){
+        $(`#track-${i}`).text(track);
+      };
+      appendTrack();
+
+      function appendSample(){
+        $(`#sample-${i}`).html(`<button id="sample-${i}}"><a href="${sample}">Play${i}</a></button>`)};
+      appendSample();
+
+    }; //end loop
+
+  };//end onSuccess function
