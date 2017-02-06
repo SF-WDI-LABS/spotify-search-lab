611e7480dc5c4169c42b8cd2981a30c7751f6815
chri-roch12 <chris.prochnow@gmail.com>
2017-02-05 16:04:05 -0800

    Added some html elements for search form, and beginnings of track search.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
+
+  console.log('JS is loaded!');
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
+var spotifySearchUrl = "https://api.spotify.com/v1/search";
+var $spotifyForm = $("form#spotify-search");
+var $trackIn = $("input#track");
+var $resultsList = $("#results");
 
+$spotifyForm.submit (function handleSubmit(event) {
+  event.preventDefault();
+
+  var info = $trackIn.val();
+
+  
+
+
+
+  });
   // your code here
 
 });
