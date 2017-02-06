1b01ece660e114bfec163e8eb3684f057221895d
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-05 21:03:54 -0800

    Added js to prevent form submit default. Reminder: Preventing form from submitting not submit button.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 // wait for DOM to load before running JS
-$(document).on('ready', function() {
+$(document).ready(function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
   // your code here
+	var endpoint = "https://api.spotify.com/v1/search"
+	var $search = $("input#search");
+	var $form = $("#spotify-track-search");
+
+
+	$form.submit(function (event) {
+		event.preventDefault();
+		console.log(event.isDefaultPrevented());
+	});
+
+
 
 });
