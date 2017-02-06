3205a66e2dfe9707c2f3c7fbe10e65c72d86f3db
kkuo57 <kkuo507@gmail.com>
2017-02-06 08:59:11 -0800

    first version

--- js/app.js
+++ js/app.js
@@ -3,7 +3,33 @@ $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
+  getTrack();
 
-  // your code here
+  $("form").on("submit", function(event) {
+    event.preventDefault(); 
+
+  getTrack();
+
+function getTrack(){}
+  $.ajax({
+    method: "GET",
+    url: "https://api.spotify.com/v1/search",
+    data: $("form").serialize(),
+    dataType: "json",
+    success: onSuccess,
+    error: onError
+  });
+}
+
+  function onSuccess(json) {
+    console.log(json);
+    $(".form-track-input")
+  }
+
+  function onError(xhr, status, errorThrown) {
+  console.log("Error: " + errorThrown);
+  console.log("Status: " + status);
+  console.dir(xhr);
+}
 
 });
