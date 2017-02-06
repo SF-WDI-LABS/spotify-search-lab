a2cf83ef8944bc242c9f5f62a1b31e88888b4718
ychoy <yanyinchoy@gmail.com>
2017-02-05 21:55:39 -0800

    added onSuccess function to append results with song name and artist

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('Sanity Check: JS is loaded!');
 
 $(document).ready(function() {
-
+  // set template literals for html elements to variables
   var $spotifySearchForm = $('#spotify-search');
   var $track = $('#query');
   var $results = $('#results');
@@ -10,11 +10,11 @@ $(document).ready(function() {
   $spotifySearchForm.on('submit', function handleFormSubmit(event) {
     event.preventDefault();
 
-    $results.empty();
+    $results.empty(); // Empty the previous results
 
     var query = $track.val();
 
-    if (query === "") {
+    if (query === "") { //if no keyword is entered
       alert("Enter a song to search");
     } else {
 
@@ -28,7 +28,36 @@ $(document).ready(function() {
     }
 
     $spotifySearchForm[0].reset();
-    $track.focus();
+    $track.focus(); // have the cursor display on the input area
 
   });
- 
+
+  function onSuccess(data) {
+    console.log("received data:", data);
+    var trackResults = data.tracks.items;
+    console.log(trackResults);
+
+    if (trackResults.length > 0) {
+      trackResults.forEach(function (result, index) {
+
+        var trackData = {
+          artist: result.artists[0].name,
+          name: result.name,
+        };
+
+        var $resultstoDisplay = `
+                <div class="row">
+                  <div class="col-xs-8">
+                    <p><strong> ${trackData.name}</strong> by ${trackData.artist}</p>
+                  </div>
+                </div>
+                <hr>
+              `;
+              $results.append($resultstoDisplay);
+        });
+    } else {
+        $results.append('<p class="text-center">No results</p>');
+      }
+    }
+
+})
