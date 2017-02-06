0e2edbe44d8eb9ba528310b67f005d4ec25eb374
monzelb <monzelb@gmail.com>
2017-02-05 21:49:28 -0800

    cleaned up code and added messages for incomplete searches

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
-// wait for DOM to load before running JS
+
 $(document).on('ready', function() {
 
-  // check to make sure JS is loaded
   console.log('JS is loaded!');
-  
-  
-  // your code here
+
 $("form").on("submit", function(event){
 	event.preventDefault();
-
 	var query= $("input#track").val();
-
+	if(query===""){
+		$(".song").remove("");
+		$('#results').append(`<p class= "song">Please enter a valid search</p>`);
+	}
 	$.ajax({ 
 		method: "GET",
 		url: "https://api.spotify.com/v1/search",
@@ -20,36 +19,31 @@ $("form").on("submit", function(event){
 		success: onSuccess,
 		error: onError
 	})
-
 })
 
 function onSuccess(json){
 	$(".song").remove("");
-	var tracks = json.tracks.items
+	var tracks = json.tracks.items;
+	if(!tracks.length){
+			$('#results').append(`<p class="song">No results found</p>`);
+		}
 	for(var i =0; i<20; i++){
 		var results= {artist: tracks[i].artists[0].name,
 				     track: tracks[i].name,
 				     image: tracks[i].album.images[0].url,
 				     preview: tracks[i].preview_url
-				 };
-		var songInfo=`<p><b>${results.track}</b> by ${results.artist}</p>`
-		var albumArt=`<img src="${results.image}">`
-		var preview= `<audio controls><source src="${results.preview}" type="audio/mpeg"></audio>`
+					 };
+		var songInfo=`<p><b>${results.track}</b> by ${results.artist}</p>`;
+		var albumArt=`<img src="${results.image}">`;
+		var preview= `<audio controls><source src="${results.preview}" type="audio/mpeg"></audio>`;
 		$("#results").append(`<div class="song">${songInfo}${albumArt}${preview}</div>`)
 		
+		
 	}
-	console.log(json.tracks.items)
-	console.log("hey");
-	
-
 }
 
 function onError(){
 	console.log("ero!");
 }
-
-
-
-
 
 });
