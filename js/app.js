57de0be4550a73b4cb97c6cd85e5d1b3155b231e
zeroCalm <smith.timothyh@gmail.com>
2017-02-05 16:23:44 -0800

    fixed minor syntax, now I can console the json object

--- js/app.js
+++ js/app.js
@@ -1,9 +1,45 @@
+$( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+
+});
+
+
+
+
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  $.ajax({
 
+    // What kind of request
+    method: "GET",
+
+    // The URL for the request
+    url: "https://api.spotify.com/v1/search?q=hello&type=track",
+
+    // The data to send aka query parameters
+    data: $("form").serialize(),
+    // Code to run if the request succeeds;
+    // the response is passed to the function
+    success: onSuccess,
+
+    // Code to run if the request fails; the raw request and
+    // status codes are passed to the function
+    error: onError
+  });
 });
+
+function onSuccess(json){
+  console.log(json);
+}
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+  }
