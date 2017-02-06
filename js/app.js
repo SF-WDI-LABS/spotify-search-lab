e14f54b5b00827a9cb3d6880a115a520bcbfda20
Daniel Thompson <19dst89@gmail.com>
2017-02-06 10:10:10 -0800

    added ajax example for reference

--- js/app.js
+++ js/app.js
@@ -4,6 +4,14 @@ $(document).on('ready', function() {
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  // temp ajax call for reference
+
+  $.ajax({
+    method: "GET",
+    url: "https://api.spotify.com/v1/search",
+    data: $("form").serialize(),
+    success: onSuccess,
+    error: onError
+  });
 
 });
