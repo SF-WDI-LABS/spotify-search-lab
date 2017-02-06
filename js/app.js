a375d6ebcb426e5001e09c42dfb841eff99cfb84
cnwilliams <hello.christenwilliams@gmail.com>
2017-02-05 21:59:25 -0800

    no errors

--- js/app.js
+++ js/app.js
@@ -1,29 +1,48 @@
+
+
 // wait for DOM to load before running JS
-$(document).on('ready', function() {
+$(document).ready(function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
-
   // your code here
 
 
-  var $trackInput = $("input.search-text-input");
-  var query = $trackInput.val();
+  var $trackInput = $("input#query");
+  var $resultsContainer = $("#results");
+  var $spotifySearchForm = $("#search-form");
 
-  $.ajax ({
-    method: 'GET',
-    url: "https://api.spotify.com/v1/search",
-    data: {
-      type: "track",
-      q: query
-    },
-    success: onSuccess,
-    error: onError
+  // ajax request happens when search button is pressed
+  $spotifySearchForm.on("submit", function searchTrackSubmit(e){
+
+    var query = $trackInput.val();
+
+    $.ajax ({
+      method: 'GET',
+      url: "https://api.spotify.com/v1/search",
+      // data: $("form").serialize(),
+      data: {
+        type: "track",
+        q: query
+      },
+      success: onSuccess,
+      error: onError
+      });
+
     });
 
   function onSuccess(data){
     console.log("received data:", data);
     console.log(data.tracks.items);
+
+// if there are no results
+  if (data.tracks.items.length == 0){
+    $resultsContainer.append("<p class='text-center'>No tracks found</p>");
+    return
+  }
+
+
+
   };
 
   function onError(xhr, status, errorThrown) {
