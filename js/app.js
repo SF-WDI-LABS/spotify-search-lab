ab1fb10aa8c38b89132a6732b6477855d5bcb9c7
chri-roch12 <chris.prochnow@gmail.com>
2017-02-05 22:36:47 -0800

    Got the data appended to the html. I used a lot of html before breking down and seeking the solutions branch.

--- js/app.js
+++ js/app.js
@@ -4,45 +4,70 @@
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
-var spotifySearchUrl = "https://api.spotify.com/v1/search";
-var $spotifyForm = $("form#spotify-search");
-var $trackIn = $("input#track");
-var $resultsList = $("#results");
+  var spotifySearchUrl = "https://api.spotify.com/v1/search";
+  var $spotifyForm = $("form#spotify-search");
+  var $trackIn = $("input#track");
+  var $resultsList = $("#results");
 
-$spotifyForm.submit (function handleSubmit(event) {
-  event.preventDefault();
+  $spotifyForm.submit (function handleSubmit(event) {
+    event.preventDefault();
 
-  var info = $trackIn.val();
+    var info = $trackIn.val();
 
-  if (info === "") {
-    alert("Dude! You gotta put something in the search field!");
-    return;
-  }
+    if (info === "") {
+      alert("Dude! You gotta put something in the search field!");
+      return;
+    }
 
-  $resultsList.empty();
-
-  $.ajax({
-    method: "GET",
-    url: spotifySearchUrl,
-    data: {
-      type: "track",
-      q: "query"
-    },
-    success: handleSpotifyOutput
+    $.ajax({
+      method: "GET",
+      url: spotifySearchUrl,
+      data: {
+        type: "track",
+        q: "query"
+      },
+      success: handleSpotifyOutput
     });
 
-  $spotifyForm.empty();
   });
 
-var handleSpotifyOutput = function(data) {
-  console.log("data check", data);
-  resultTracker = data.tracks.items;
+  var handleSpotifyOutput = function(data) {
+    //console.log("data check", data);
+    var resultTracker = data.tracks.items;
+    //console.log(resultTracker); This took me forever to troubleshoot. extra letter.
 
-  if (!resultTracker.length) {
-    $resultsList.append('<p class="text-center">No results</p>');
-    return;
-  };
-};
-  // your code here
+    if (!resultTracker.length) {
+      $resultsList.append('<p class="text-center">No results</p>');
+      return;
+    }
+
+    var putResultToPage = resultTracker.map(function (results, index) {
+      var infoTracker = {
+        albumArt: results.album.images.length > 0 ? results.album.images[0].url : null,
+        artist: results.artists[0].name,
+        name: results.name,
+        previewUrl: results.preview_url
+    };
+// I didnt think of this, and tried creating an html scaffolding to append data to.
+// The album art was also more than I could handle. Used the solutions branch.
+    return (`
+        <div class="row">
+          <div class="col-xs-4">
+            <img src="${infoTracker.albumArt}" class="img-responsive">
+          </div>
+          <div class="col-xs-8">
+            <p><strong> ${infoTracker.name}</strong> by ${infoTracker.artist}</p>
+            <p>
+              <a href="${infoTracker.previewUrl}" target="_blank" class="btn btn-sm btn-default">
+                Preview <span class="glyphicon glyphicon-play"></span>
+              </a>
+            </p>
+         </div>
+        </div>
+        <hr>
+        `);
+    });
+    $resultsList.html(putResultToPage);
+  }
 
 });
