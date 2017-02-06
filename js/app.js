ede16ffa62e33b6abd8f241303f57c9c3dfc326b
ychoy <yanyinchoy@gmail.com>
2017-02-05 21:40:46 -0800

    added AJAX

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
-// wait for DOM to load before running JS
-$(document).on('ready', function() {
+console.log('Sanity Check: JS is loaded!');
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
+$(document).ready(function() {
 
-  // your code here
+  var $spotifySearchForm = $('#spotify-search');
+  var $track = $('#query');
+  var $results = $('#results');
+  var $loading = $('#loading');
 
-});
+  $spotifySearchForm.on('submit', function handleFormSubmit(event) {
+    event.preventDefault();
+
+    $results.empty();
+
+    var query = $track.val();
+
+    if (query === "") {
+      alert("Enter a song to search");
+    } else {
+
+      var songSearchUrl = 'https://api.spotify.com/v1/search?type=track&q=' + query;
+
+      $.ajax({
+        method: 'GET',
+        url: songSearchUrl,
+        success: onSuccess
+      });
+    }
+
+    $spotifySearchForm[0].reset();
+    $track.focus();
+
+  });
+ 
