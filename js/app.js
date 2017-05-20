7ddec0e393949c7555689469d0c4805632a17a58
alyshae <alison.walas@gmail.com>
2017-05-19 17:46:03 -0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+var endpoint = "https://api.spotify.com/v1/search";
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
-
   // check to make sure JS is loaded
-  console.log('JS is loaded!');
+  //console.log('JS is loaded!');
 
-  // your code here
+  function getData() {
+    $.ajax({
+      method: "GET",
+      url: endpoint;
+      success: onSuccess
+    });
+  }
+
+  function onSuccess(resultData) {
+
+  }
 
 });
