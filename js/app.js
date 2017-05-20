96e8293cf20f4948a1149912a0f71d189701f8ab
Kat Tsukuda <BearHeartsBat@Kats-MBP.mhs>
2017-05-20 11:44:37 -0700

    html and AJAX call for Spotify search set up

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ $(document).on('ready', function() {
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  // AJAX calling Spotify API
+  $.ajax({
+      method: "GET",
+      url:"https://api.spotify.com/v1/search?type=track&=q",
+      success: (data) => console.log(data)
+
+  });
 
 });
