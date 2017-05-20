bb0052790078c440f2ce211424625bdafb40e8b4
alyshae <alison.walas@gmail.com>
2017-05-19 18:25:57 -0700

    getting set up. Successfully console.logged an object of data from spotify's api.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-var endpoint = "https://api.spotify.com/v1/search";
+
 
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
-  // check to make sure JS is loaded
   //console.log('JS is loaded!');
+  var endpoint = "https://api.spotify.com/v1/search";
+  getData();
 
   function getData() {
     $.ajax({
       method: "GET",
-      url: endpoint;
+      url: endpoint,
+      data: {
+        type: "track",
+        q: "query"
+      },
       success: onSuccess
     });
   }
 
   function onSuccess(resultData) {
-
+    console.log(resultData);
   }
 
 });
