0920c1d5f50ca4178c05355041f317b4a467fe4f
spragala <spragala@yahoo.com>
2017-05-22 09:16:31 -0700

    spotify!

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
+// GLOBALS
+let spotify_endpoint = "https://api.spotify.com/v1/search"
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
+// CLICK EVENT
+  $(".form-inline").on("submit", function(event) {
+    event.preventDefault();
 
-  // your code here
+// GET INFO FROM SPOTIFY API
+    $.ajax({
+      method: "GET",
+      url: spotify_endpoint,
+      data: $("input").serialize(),
+      success: function(response) {
+
+// CLEAR SEARCH FIELD
+        $('#results').html("");
+
+        let responseArray = response.artists.items;
+
+        responseArray.forEach( function(i) {
+          let pic   = i.images[2].url;
+          let name  = i.name
+          let link  = i.external_urls.spotify
+
+          $("#results").append(`<li class="list-group-item"><a href="${link}"><img src="${pic}">${name}</a></li>`)
+        })
+
+      }
+    })
+  })
 
 });
