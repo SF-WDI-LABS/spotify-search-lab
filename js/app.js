dff04df0d8c057d383db26be1264b7ef17937688
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-08-26 16:13:20 -0700

    set up ajax call and set up append of track name and artist name to results div

--- js/app.js
+++ js/app.js
@@ -3,7 +3,22 @@ $(document).ready( function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
-
-  // your code here
+  $.ajax({
+    type: "GET",
+    url: "https://api.spotify.com/v1/search",
+    data:{
+      // q: $("input").val(),
+      q: "get back",
+      type: "track"
+    },
+    headers:{
+      "Authorization": "Bearer BQAxv03xFbbfZ0Bwqzf21JuO-6Uxlfu-adAb5KXedoFzxjgl-eeVmbbwK1Yd7oc71sFnXZVFh1qw-Fyuu4iMBw"
+    }
+  }).then(function(data){
+    data.tracks.items.forEach(function(element){
+      $("#results").append(`<p>${element.name} by ${element.album.artists[0].name}</p>`);
+    })
+  })
+  // your code her
 
 });
