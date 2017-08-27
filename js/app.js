af96665895095f3881e217656b9fcd361c953841
Huan Ming Liao <hmliao14@gmail.com>
2017-08-26 19:49:00 -0700

    Added all min/bonus requirements except loading.gif

--- js/app.js
+++ js/app.js
@@ -6,47 +6,82 @@ $(document).ready( function() {
 
   // your code here
 
-  function song_template(image_url, title, artist){
+  function song_template(image_url, title, artists, preview_url){
+  	var preview = "";
+  	// Make the link visible if its not null
+  	// if the url is null, by default it will be a '#'
+  	if (preview_url === null || preview_url === undefined ) {
+  		preview_url = '#';
+  	} else preview = ` <audio controls>
+							  		<source src="${preview_url}" type="audio/mpeg">
+							 </audio> `
+							  	
   	return 	`
   	<div class="col-4">
-  	<div class="card">
-	  	<img class="card-img-top" src="${image_url}" alt="song_image">
-	  	<div class="card-body">
-	  	<h4 class="card-title">${title}</h4>
-	  	<p class="card-text">Artist(s) : ${artist}</p>
-  	</div>
+	  	<div class="card">
+		  	<img class="card-img-top" src=${image_url} alt="song_image">
+		  	<div class="card-body">
+		  	<h4 class="card-title">${title}</h4>
+		  	<p class="card-text">By ${artists}</p>
+		  	${preview}
+		  	</div>
+	  	</div>
   	</div>		  
   	`
   };
 
-  var query = "";
+  var $query = $("input[type=text]");
+  var $result = $("#results");
+  var input = "";
 
   $(".search_form").on("submit", function(event){
   	event.preventDefault();
 
-  	if($("input[type=text]").eq(0).val() !== "") query = $("input[type=text]").eq(0).val();
+  	if($query.eq(0).val() !== "") input = $query.eq(0).val();
   	else { 
-  		console.log("Please enter a title");
+  		alert("Please enter a title");
   		return;
   	}
   	$.ajax({
   		type: "GET",
   		url: `https://api.spotify.com/v1/search`,
   		data: {
-  			q: query,
+  			q: input,
   			type: "track",
   			limit: 50
   		},
   		headers: {
 	  		// Bearer yourTokenFromWebApp
-	  		"Authorization": "Bearer BQCFdiHHrDRcErE3X6NMVUdpqMx8yOh76638aacqa-uZO_DXoH17ZRfxao9lAJ_IAd_Zxm1JLHAYNHJ7BpIRAw"
+	  		"Authorization": "Bearer BQCbOZHApC7hNRRS7e1LN-1Jt6oejkoFTleZedseXltTpXggw_4nXyHjQbNs_RscPn0CuV9pRqfU4OknD1aYVg"
 	  	}
 	  }) .then(function(response){
 	  	console.log(response);
-	  	// Filter the tracks to titles and append them onto the page
-	  	response.forEach(function(item){
+	  	var items = response.tracks.items;
+	  	if(items.length === 0) {
+	  		alert("There are no matched results");
+	  		return;
+	  	}
+	  	// Clear the fields, filter the tracks to titles 
+	  	// and append them onto the page\
+	  	$query.eq(0).val("");
+	  	$result.empty();
+	  	var artists = "";
+	  	items.forEach(function(item){
+		  	for(var i =0; i< item.artists.length; i ++){
+		  		artists =  item.artists[i].name + ", " + artists;
+		  	}
+		  	$result.append(song_template(	item.album.images[1].url,
+		  							item.name,
+		  							artists.slice(0,artists.length-2),
+		  							item.preview_url));
+		  	artists = "";
+	  	});
+	  
+	  	// for(var i =0; i < items.length; i ++){
 
-	  	})
+	  	// 	$result.append(`<p> ${items[i].artists[0].name}</p>`);
+	  	// }
+	  	//$result.append
 
 	  }).catch(function(error){
 	  	console.log(error);
