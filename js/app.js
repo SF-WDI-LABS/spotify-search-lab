4a7ae7e83152c05d98894ca44f1ba6a67f6c6675
Anthony-g2 <ant.greenwell2@gmail.com>
2017-08-27 01:19:40 -0700

    track results

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ $(document).ready( function() {
 
 
   // your code here
+
+  // function tempTrack (tracks){
+  //   return `<div class='container inline'>
+  //     <img src='${tracks.tracks.items[i].album.images[1].url}'>
+  //     <p>Artist - ${tracks.tracks.items[i].album.artists[0].name} </p>
+  //     <p>Album - ${tracks.tracks.items[i].album.name} </p>
+  //     <p>Popularity - ${tracks.tracks.items[i].album.popularity} </p>
+  //    </div>`)
+  // }
+
   $('#spSearch').submit(function(event){
     event.preventDefault();
     let name = $('#qName').val();
@@ -15,15 +25,31 @@ $(document).ready( function() {
       url: "https://api.spotify.com/v1/search",
       data: {
         q: name,
-        type: type
+        type: type,
+        limit: 10
       },
       headers: {
-        "Authorization": "Bearer BQBHyYTxQNw__OenpoWCSyHDFEOG5MrTmW-cYX-83QfB18fafkGxLCtHTThlYw7Oijc2zmh-VULq07eNOUfHIg"
+        "Authorization": "Bearer BQC4X3DOAIwUvXHHVdKmqAqpLk8w1OzlkalMj6-FRvwX7E3JkoZ6jgoxVjSCROGBni3chNluv8EoR9qcpzUiKA"
       }
     })
     .then(function(data){
       console.log(data);
-
+      if (type = 'tracks'){
+        for (let i=0;i < data.tracks.items.length;i++){
+          $('#results').append(
+            `<div class='container'>
+              <div class="resShow">
+                <img src='${data.tracks.items[i].album.images[1].url}'>
+                  <ul>
+                    <li>Track Name - ${data.tracks.items[i].name}</li>
+                    <li>Artist - ${data.tracks.items[i].album.artists[0].name}</li>
+                    <li>Album - ${data.tracks.items[i].album.name}</li>
+                  </ul>
+                </div>
+                <hr>
+              </div>`
+        )};
+      }
     });
     $('#spSearch').trigger('reset');
     $('#resTitle').text(`${type}s - "${name}"`);
