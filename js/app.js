5f7d2f9a322f970b1c2f81541ca366b1cf1afbe9
balbini <balbini91@gmail.com>
2017-08-26 18:25:15 -0700

    data pull from api working

--- js/app.js
+++ js/app.js
@@ -5,5 +5,50 @@ $(document).ready( function() {
   console.log('JS is loaded!');
 
   // your code here
+  // let data = function(response){
+  //   let $results = $("#results");
+  //
+  //     $results.empty()
+  // }
+
+let userSearch = $("#mySpotifySearch").val();
+
+
+    $("#searchForm").on("submit",function(event){
+      event.preventDefault()
+
+      let userSearch = $("#mySpotifySearch").val();
+
+      $.ajax({
+        type: "GET",
+        url:"https://api.spotify.com/v1/search",
+        data: {
+          query:userSearch,
+          type: "track",
+          limit: 10
+        },
+        headers: {
+          "Authorization":"Bearer BQCmBEQF2rXUmsNHo4oNHuSYdKacDlneg3fEv_OWYV7fGHYCQNh2aDdjnNHHnN7NL5rgYn_TQyOoBXZY_knHhA"
+        }
+      })
+        .then(function(data){
+          console.log(data);
+        })
+        .catch(function(err){
+          console.log("err");
+        })
+
+    })
+
+    // let pageUpdate = function(response){
+    //   let $results = $("#results");
+    //
+    //   $results.empty();
+    //
+    //   response.data forEach(function(artist){
+    //     let newHTML =
+    //   })
+
+
 
 });
