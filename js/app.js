277a3c1f73fb4af2f6751b128136c05d3815e45c
David <davjue@gmail.com>
2017-08-28 00:24:49 -0700

    added album option, playlist and artist may be done at a later time, added no search found message

--- js/app.js
+++ js/app.js
@@ -4,11 +4,21 @@ $(document).ready( function() {
   // https://spotify-token-finder.herokuapp.com/
   let query = $(".search-bar").val();
   let type = $('.select-type').val();
+
   $(".submit").on("click", function(event) {
     event.preventDefault();
     query = $(".search-bar").val();
     type = $(".select-type").val();
     grabJson(query, type);
+  });
+
+  $(window).keydown(function(event) {
+      if (event.keyCode === 13) {
+          event.preventDefault();
+          query = $(".search-bar").val();
+          type = $(".select-type").val();
+          grabJson(query, type);
+      }
   });
 
   let grabJson = (query, type)=>
@@ -23,11 +33,11 @@ $(document).ready( function() {
         // limit: 20
       },
       headers: {
-        Authorization: "Bearer BQAO_Zjb7DDcy0Jz9D0NUfwWxBb5C7MzLFyNxIkOqOgh05B7hQDe03qC2Ue9nORM7L1F5o1vMJ4x8Kv73EGFPg"
+        Authorization: "Bearer BQBwkYoZwsRvGL2a47mVJP9E3LKjREs9OGAZoFjNQp6qoCbZK_USFvV_5u0Jgpbp_uSgRa4LmTIXl-EIhlmsBQ"
       }
     })
     .then(function(response) {
-      searchMap = {
+      let searchMap = {
           album: "albums",
           artist: "artists",
           playlist: "playlists",
@@ -36,20 +46,72 @@ $(document).ready( function() {
       console.log(response);
       console.log(type);
       $('.results-row1').html("");
-      for (let i = 0; i<response[searchMap[type]].items.length; i++) {
-          console.log(response[searchMap[type]].items[0].external_urls.spotify);
-          $('.results-row1').append(`
-            <div id="results">
-              <figure class = "figures col-sm-4">
-                <a href="${response[searchMap[type]].items[0].external_urls.spotify}">
-                  <img class="img-album" src=${response[searchMap[type]].items[i].album.images[1].url}>
-              </figure>
-            </div>
-            `
-        );
-      };
+      if (response[searchMap[type]].items.length > 0) {
+          for (let i = 0; i<response[searchMap[type]].items.length; i++) {
+              if (type === "track" && response[searchMap[type]].items[i].album.images[1].url.length > 0
+                  && response[searchMap[type]].items[i].album.images.length > 0) {
+                  appendTrack(response, searchMap, i);
+              }
+              else if (type === "album") {
+                  appendAlbum(response, searchMap, i);
+              }
+              // else if (type === "playlist" && (response[searchMap[type]].items[i].images[0].height === 300 ||
+              //         response[searchMap[type]].items[i].images[1].height === 300)) {
+              //     appendPlaylist(response, searchMap, i);
+              // }
+
+
+          };
+      }
+      else {
+          $('.results-row1').append(`<h1>No results found</h1>`);
+      }
+
     })
     .catch(function(err) {
       console.log(err);
     });
+
+  let appendTrack = (response, searchMap, i) => {
+      $('.results-row1').append(`
+            <div id="results">
+              <figure class = "figures col-sm-4">
+                <a href="${response[searchMap[type]].items[i].external_urls.spotify}">
+                  <img class="img-album" src=${response[searchMap[type]].items[i].album.images[1].url}>
+                <figcaption>
+                  ${response[searchMap[type]].items[i].artists[0].name} - ${response[searchMap[type]].items[i].name}
+                </figcaption>
+              </figure>
+            </div>`
+      );
+  };
+
+  let appendAlbum = (response, searchMap, i) => {
+      $('.results-row1').append(`
+          <div id="results">
+            <figure class = "figures col-sm-4">
+              <a href="${response[searchMap[type]].items[i].external_urls.spotify}">
+                <img class="img-album" src=${response[searchMap[type]].items[i].images[1].url}>
+              <figcaption>
+                ${response[searchMap[type]].items[i].artists[0].name} - ${response[searchMap[type]].items[i].name}
+              </figcaption>
+            </figure>
+          </div>`
+      );
+  }
+
+  //Will add later once I figure out how to loop through the size of the images
+  // let appendPlaylist = (response, searchMap, i) => {
+  //     $('.results-row1').append(`
+  //       <div id="results">
+  //         <figure class = "figures col-sm-4">
+  //           <a href="${response[searchMap[type]].items[i].external_urls.spotify}">
+  //             <img class="img-album" src=${response[searchMap[type]].items[i].images[0].url}>
+  //           <figcaption>
+  //             ${response[searchMap[type]].items[i].name}
+  //           </figcaption>
+  //         </figure>
+  //       </div>`
+  //     );
+  // }
 });
