522f077d32854f74ed43461db36910c1565687f3
Anthony-g2 <ant.greenwell2@gmail.com>
2017-08-28 00:17:57 -0700

    bug fixes

--- js/app.js
+++ js/app.js
@@ -18,41 +18,56 @@ $(document).ready( function() {
 
   $('#spSearch').submit(function(event){
     event.preventDefault();
-    let name = $('#qName').val();
-    let type = $('input.qType:checked').val();
+    let $name = $('#qName').val();
+    let $type = $('input.qType:checked').val();
     $.ajax({
       method: 'GET',
       url: "https://api.spotify.com/v1/search",
       data: {
-        q: name,
-        type: type,
+        q: $name,
+        type: $type,
         limit: 10
       },
       headers: {
-        "Authorization": "Bearer BQC4X3DOAIwUvXHHVdKmqAqpLk8w1OzlkalMj6-FRvwX7E3JkoZ6jgoxVjSCROGBni3chNluv8EoR9qcpzUiKA"
+        "Authorization": "Bearer BQBHJT3zE3L3HXLa_X7U94DH4Zqkid2Va_XVm3qWEJfq4WbOKfaIU-2ohwOgXqyFbrd5hebQfxmSU1UgW3YUvw"
       }
     })
     .then(function(data){
-      console.log(data);
-      if (type = 'tracks'){
+      $('#results').html('');
+      $('#resTitle').fadeIn(1);
+      if ($type = 'tracks'){
         for (let i=0;i < data.tracks.items.length;i++){
           $('#results').append(
             `<div class='container'>
+              <hr style="width: auto; margin-left: -30px">
               <div class="resShow">
                 <img src='${data.tracks.items[i].album.images[1].url}'>
                   <ul>
                     <li>Track Name - ${data.tracks.items[i].name}</li>
-                    <li>Artist - ${data.tracks.items[i].album.artists[0].name}</li>
+                    <br>
                     <li>Album - ${data.tracks.items[i].album.name}</li>
+                    <br>
+                    <li>Artist - ${data.tracks.items[i].album.artists[0].name}</li>
+                  </ul>
+                </div>
+              </div>`
+        )};
+      } else if ($type = 'artist'){
+          $('#results').append(
+            `<div class='container'>
+              <div class="resShow">
+                <img src='${data.artists.items[0].images[1].url}'>
+                  <ul>
+                    <li>Artist - ${data.artists.items[0].name}</li>
+                    <li>Genre - ${data.artists.items[0].genres[0]}</li>
+                    <li>Popularity - ${data.artists.items[0].popularity}/100</li>
                   </ul>
                 </div>
                 <hr>
               </div>`
         )};
-      }
-    });
-    $('#spSearch').trigger('reset');
-    $('#resTitle').text(`${type}s - "${name}"`);
+      });
+    $name = null;
+    $type = null;
   });
-
 });
