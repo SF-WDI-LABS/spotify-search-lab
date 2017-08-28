ff38d1e9da2b5864db60f5aba8192d25d399d513
ellismitchell <ellisgmitchell@gmail.com>
2017-08-28 09:12:31 -0700

    finished hw

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 // wait for DOM to load before running JS
 $(document).ready( function() {
-
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
-
   // your code here
-
+  let accessToken = 'BQAKLPbNxGlO2XBbJszxwbnorYw8X8tNMmw1Pax56wNguvLlp5pHdAdQI2qeNPZd2bA2cRC3casWM3YumlQDNA';
+  
+  $('#search').on('submit', function(event) {
+  	event.preventDefault();
+  	let q = $('#q').val();
+  	$.ajax({
+  		url: `https://api.spotify.com/v1/search?q=${q}&type=track`,
+  		headers: {
+  			'Authorization' : 'Bearer '+accessToken
+  		}
+  	})
+  	.then(function(data){
+  		let $results = $('#results');
+  		$results.html('');
+  		data.tracks.items.forEach(function(song){
+  			$results.append('<p>'+song.name+' - '+song.artists[0].name+'</p>');
+  		});
+  	})
+  	.catch(function(error){
+  		console.log(error);
+  	});
+  });
 });
