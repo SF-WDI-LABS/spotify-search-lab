dfadf2d950d119ea57d9f95d1bbe2acc2db2d923
Swetha Muralidharan <swethamuralidharan@Swethas-MacBook-Pro.local>
2017-12-11 10:50:19 -0800

    created index and app.js files

--- js/app.js
+++ js/app.js
@@ -3,7 +3,28 @@ $(document).ready( function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
-
+//var url_link="https://developers.zomato.com/api/v2.1/cities";
+var url_link="https://api.tvmaze.com/shows?q=";
   // your code here
-
+$("#search_btn").on('click',function(){
+//url_link=url_link+"?q="+$("#input_id").val();
+  alert(url_link);
+  //  e.preventDefault();
+    $.ajax({
+      method: 'GET',
+      url: url_link,
+      dataType: 'json',
+      data:$("form").serialize(),
+      success: onSuccess,
+       error:onError
+    })
+});
+function onSuccess(data){
+  alert(url_link);
+  console.log(data);
+}
+function onError(data){
+  alert(`error :${data}`);
+//alert("error");
+}
 });
